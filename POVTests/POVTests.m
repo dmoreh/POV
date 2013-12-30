8eb016d6d2124f674dc2708f24379f6b9950cb55
Daniel Moreh <danielmoreh@Daniels-MacBook-Air-2.local>
2013-12-30 17:16:52 +0200

    Add tests.

---
+++ POVTests/POVTests.m
@@ -0,0 +1,34 @@
+//
+//  POVTests.m
+//  POVTests
+//
+//  Created by Daniel Moreh on 12/30/13.
+//  Copyright (c) 2013 Daniel Moreh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface POVTests : XCTestCase
+
+@end
+
+@implementation POVTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
